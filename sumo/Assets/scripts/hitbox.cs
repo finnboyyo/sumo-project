3b089e9fda2d8360d7ac023a95db305b9814500b
finnboyyo <44878824+finnboyyo@users.noreply.github.com>
2018-11-08 14:01:48 -0800

    hitbox made

---
+++ sumo/Assets/scripts/hitbox.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hitbox : MonoBehaviour {
+	[SerializeField]bool beenHit;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Collider> ().isTrigger = true;
+	}
+	
+	void OnTriggerStay (Collider other) {
+		if (beenHit == false) {
+			beenHit = true;
+			Reaction (other);
+		} 
+	}
+
+	void Reaction (Collider other) {
+		Debug.Log (other.gameObject.name);
+	}
+	void OnEnable (){
+		Debug.Log ("yo");
+		beenHit = false;
+	}
+}
